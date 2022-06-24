07b1ff003daac59afc4ab8422fe0c86b08f627fa
mattfwilson2 <mattfwilson+1@gmail.com>
2022-06-24 00:32:01 -0400

    Renamed make crate open function and removed old if statements that could be done more efficiently

--- crates.py
+++ crates.py
@@ -37,7 +37,7 @@ crateItems = {
 item_painted = ['Black', 'Burnt Sienna', 'Cobalt', 'Crimson', 'Forest Green', 'Grey', 'Lime', 'Orange', 'Pink', 'Purple', 'Saffron', 'Sky Blue', 'Titanium White']
 item_certified = ['Acrobat', 'Aviator','Goalkeeper', 'Guradian', 'Juggler', 'Paragon', 'Playmaker', 'Scorer', 'Show-Off', 'Sniper', 'Striker', 'Sweeper', 'Tactician', 'Turtle', 'Victor']
 
-def getItem(rarity, painted, certified):
+def openCrate(rarity, painted, certified):
     listRoll = []
     roll = random.choice(rarity)
     rollPainted = random.choice(item_painted)
