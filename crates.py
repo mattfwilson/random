86bd3e4c9055f4a0a133b7987b0eeca42eb60d0b
mattfwilson <mattfwilson@gmail.com>
2022-06-25 00:27:29 -0400

    Moved crate dictionary and painted and certified extra lists inside the main crate open function

--- crates.py
+++ crates.py
@@ -8,37 +8,38 @@
 
 import random
 
-crateItems = {
-    'Dragon Lord': [1, 'Decal', False, True],
-    'Funny Book': [1, 'Decal', False, True],
-    'Lone Wolf': [1, 'Decal', False, True],
-    'StarLighter': [1, 'Decal', False, True],
-    'Pearlescent Matte': [1, 'Paint Finish', False, True],
-
-    'Chakram': [2, 'Wheels', True, True],
-    'Reaper': [2, 'Wheels', True, True],
-    'Gaiden': [2, 'Wheels', True, True],
-    'Yamane': [2, 'Wheels', True, True],
-
-    'Jäger 619 RS': [3, 'Body', True, True],
-    'Twinzer': [3, 'Body', True, True],
-    'Comet': [3, 'Rocket Boost', False, True],
-
-    'Draco': [4, 'Wheels', True, True],
-    'Zomba': [4, 'Wheels', True, True],
-    'Infinium': [4, 'Wheels', True, True],
-
-    'Dissolver': [5, 'Animated Decal', False, True],
-    'Fire God': [5, 'Animated Decal', False, True],
-    'Mainframe': [5, 'Animated Decal', False, True],
-    'Solar Flare': [5, 'Goal Explosion', True, True],
-}
-
-item_painted = ['Black', 'Burnt Sienna', 'Cobalt', 'Crimson', 'Forest Green', 'Grey', 'Lime', 'Orange', 'Pink', 'Purple', 'Saffron', 'Sky Blue', 'Titanium White']
-item_certified = ['Acrobat', 'Aviator','Goalkeeper', 'Guradian', 'Juggler', 'Paragon', 'Playmaker', 'Scorer', 'Show-Off', 'Sniper', 'Striker', 'Sweeper', 'Tactician', 'Turtle', 'Victor']
-
 def openCrate(rarity, painted, certified):
-    listRoll = []
+
+    crate_roll = []
+
+    players_choice = {
+        'Dragon Lord': [1, 'Decal', False, True],
+        'Funny Book': [1, 'Decal', False, True],
+        'Lone Wolf': [1, 'Decal', False, True],
+        'StarLighter': [1, 'Decal', False, True],
+        'Pearlescent Matte': [1, 'Paint Finish', False, True],
+
+        'Chakram': [2, 'Wheels', True, True],
+        'Reaper': [2, 'Wheels', True, True],
+        'Gaiden': [2, 'Wheels', True, True],
+        'Yamane': [2, 'Wheels', True, True],
+
+        'Jäger 619 RS': [3, 'Body', True, True],
+        'Twinzer': [3, 'Body', True, True],
+        'Comet': [3, 'Rocket Boost', False, True],
+
+        'Draco': [4, 'Wheels', True, True],
+        'Zomba': [4, 'Wheels', True, True],
+        'Infinium': [4, 'Wheels', True, True],
+
+        'Dissolver': [5, 'Animated Decal', False, True],
+        'Fire God': [5, 'Animated Decal', False, True],
+        'Mainframe': [5, 'Animated Decal', False, True],
+        'Solar Flare': [5, 'Goal Explosion', True, True],
+    }
+
+    item_painted = ['Black', 'Burnt Sienna', 'Cobalt', 'Crimson', 'Forest Green', 'Grey', 'Lime', 'Orange', 'Pink', 'Purple', 'Saffron', 'Sky Blue', 'Titanium White']
+    item_certified = ['Acrobat', 'Aviator','Goalkeeper', 'Guradian', 'Juggler', 'Paragon', 'Playmaker', 'Scorer', 'Show-Off', 'Sniper', 'Striker', 'Sweeper', 'Tactician', 'Turtle', 'Victor']
 
     randItem = random.randint(1, 100)
     randPainted = random.randint(1, 100)
@@ -47,20 +48,17 @@ def openCrate(rarity, painted, certified):
     roll = random.choice(rarity)
     rollPainted = random.choice(item_painted)
     rollCertified = random.choice(item_certified)
-    listRoll.append(roll)
+    crate_roll.append(roll)
     if painted <= 25:
-        listRoll.append(rollPainted)
+        crate_roll.append(rollPainted)
         print(f'{roll}, PAINTED: {rollPainted}')
     elif certified <= 25:
-        listRoll.append(rollCertified)
+        crate_roll.append(rollCertified)
         print(f'{roll}, CERTIFIED: {rollCertified}')
     elif painted <= 25 and certified <= 25:
-        listRoll.append(rollPainted)
-        listRoll.append(rollCertified)
+        crate_roll.append(rollPainted)
+        crate_roll.append(rollCertified)
         print(f'{roll}, PAINTED: {rollPainted}, CERTIFIED: {rollCertified}')
     else:
         print(f'{roll}')
-    return listRoll
-
-
-print(randItem)
+    return crate_roll
